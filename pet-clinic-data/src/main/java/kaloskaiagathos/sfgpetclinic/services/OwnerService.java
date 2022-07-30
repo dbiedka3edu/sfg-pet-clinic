21f321a742f80751ca5f25d8ecc6cb6cad6a484c
danbie <danbie@st.amu.edu.pl>
2022-07-30 16:41:01 +0200

    Impl Map based Services, closes #6

--- pet-clinic-data/src/main/java/kaloskaiagathos/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/kaloskaiagathos/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,8 @@ package kaloskaiagathos.sfgpetclinic.services;
 
 import kaloskaiagathos.sfgpetclinic.model.Owner;
 
-import java.util.Set;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
 
 public interface OwnerService extends CrudService<Owner, Long> {
 
