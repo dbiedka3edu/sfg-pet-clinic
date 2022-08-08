5a9a382367f4201676bf45ce5a574153e791feba
danbie <danbie@st.amu.edu.pl>
2022-08-08 18:22:38 +0200

    Broken links fixed . closes #45

--- pet-clinic-web/src/main/java/kaloskaiagathos/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kaloskaiagathos/sfgpetclinic/controllers/OwnerController.java
@@ -25,5 +25,10 @@ public class OwnerController {
         return "owners/index";
     }
 
+    @RequestMapping("/owners/find")
+    public String findOwners(){
+
+        return "notimplemented";
+    }
 
 }
