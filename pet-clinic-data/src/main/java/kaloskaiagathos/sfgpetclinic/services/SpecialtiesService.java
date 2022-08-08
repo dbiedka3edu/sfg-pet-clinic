8a7deae59573a800ff0f583c1406fe1221d718db
danbie <danbie@st.amu.edu.pl>
2022-08-08 16:58:11 +0200

    Added Speciality Map Service Impl  . closes #31

---
+++ pet-clinic-data/src/main/java/kaloskaiagathos/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package kaloskaiagathos.sfgpetclinic.services;
+
+import kaloskaiagathos.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
